8edc97484832c4a51e1f197afc2cfdecc763e569
BhargavMaru07 <bhargavhamir@gmail.com>
2024-11-09 00:34:15 +0530

    HeroSection,About,ContactUs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import CountryData from "../api/CountryData.json"
 
 function About() {
-  return <h1>About</h1>;
+  return (
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here are the Interesting Facts <br /> we're proud of
+      </h2>
+
+      <div className="gradient-cards">
+       {CountryData.map((data)=>{
+          const { id, countryName, capital, population, interestingFact } = data;
+          return (
+            <div className="card" key={id}>
+              <div className="container-card bg-yellow-box">
+                <p className="card-title">{countryName}</p>
+                <p>
+                  <span className="card-description">Capital:</span>
+                  {capital}
+                </p>
+                <p>
+                  <span className="card-description">Population:</span>
+                  {population}
+                </p>
+                <p>
+                  <span className="card-description">Interesting Fact:</span>
+                  {interestingFact}
+                </p>
+              </div>
+            </div>
+          );
+       })}
+      </div>
+    </section>
+  )
 }
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+
+      <div className="contact-wrapper container">
+        <form action="">
+          <input
+            type="text"
+            required
+            autoComplete="false"
+            placeholder="Enter your name"
+            name="username"
+            className="form-control"
+          />
+          <input
+            type="email"
+            required
+            autoComplete="false"
+            placeholder="Enter your email"
+            name="email"
+            className="form-control"
+          />
+          <textarea
+            required
+            autoComplete="false"
+            rows={7}
+            placeholder="Enter your message"
+            name="message"
+            className="form-control"
+          />
+      <button type="submit" className="">
+        Send
+      </button>
+        </form>
+      </div>
+
+    </section>
+  );
 }
 
 export default Contact;
