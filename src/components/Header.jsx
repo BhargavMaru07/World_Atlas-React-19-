feea1535aba63e0a38597a2565316c9a3d19eccd
BhargavMaru07 <bhargavhamir@gmail.com>
2024-11-08 21:48:06 +0530

    Home Page and Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <h1>This is Header</h1>
-  )
+    <header>
+      <div className="container">
+        <div className="grid navbar-grid">
+          <div className="Logo">
+            <NavLink to="/">
+              <h1>WorldAtlas</h1>
+            </NavLink>
+          </div>
+
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) => (isActive ? "blue" : null)}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) => (isActive ? "blue" : null)}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/country"
+                  className={({ isActive }) => (isActive ? "blue" : null)}
+                >
+                  Country
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) => (isActive ? "blue" :null)}
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
